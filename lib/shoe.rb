a202927c6c79f2cd02fd424b08fe0e4450746006
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-27 14:40:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,4 +2,13 @@ class Shoe
   def initialize(brand)
     @brand = brand
   end
+  attr_accessor :brand
+  attr_accessor :color
+  attr_accessor :size
+  attr_accessor :material
+  attr_accessor :condition
+  def cobble
+    puts "The shoe has been repaired"
+    @condition = new
+  end
 end
